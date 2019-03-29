39ba076d8deb9eedfb4dd262e9c7a765b4f24320
Patel <a_patel73563@tss-hr420-dm25.fc.ca>
2019-03-29 15:18:14 -0400

    added sass

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
     user: "root",
-    password: "",
-    port: 3306,  //windows : 3306 mac : 8889
+    password: "root",
+    port: 8889,  //windows : 3306 mac : 8889
     host: "localhost",
-    database: "db_favorite"
+    database: "db_favourite"
 }
 
 module.exports = config;
